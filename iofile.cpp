c67c776077a48103be3528ce460173d7b4baabd2
iqbalgn72 <iqbalgn72@gmail.com>
2024-06-04 14:38:25 +0700

    3

--- iofile.cpp
+++ iofile.cpp
@@ -15,4 +15,8 @@ int main() {
     cout << ">= Menulis file \'q\' untuk keluar" << endl;
 
     //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+    }
 }
