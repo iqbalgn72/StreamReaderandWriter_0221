880f97e60c7b2576421929e4ca5826ee3176cbe3
iqbalgn72 <iqbalgn72@gmail.com>
2024-06-04 14:44:29 +0700

    5

--- iofile.cpp
+++ iofile.cpp
@@ -20,6 +20,9 @@ int main() {
         //mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
         //loop akan berhenti jika anda memasukan karakter q
-        
-    }
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari baris ke dalam file
+        outfile << baris << endl;
+    } 
+    //selesai dalam menulis sekarang tutup filenya
 }
