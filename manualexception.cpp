6e7face5089a95010c197f61deb2fa7c5c1b47c9
iqbalgn72 <iqbalgn72@gmail.com>
2024-06-04 15:21:45 +0700

    manual

---
+++ manualexception.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    try {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a){
+        //blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...){
+        //jika selain integer maka blok ini akan dieksekusi/
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
+}
